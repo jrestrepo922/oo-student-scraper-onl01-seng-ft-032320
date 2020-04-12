5ea3086db8da8ca9716aaefdfbec491701959219
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-12 13:26:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    url = https://learn-co-curriculum.github.io/student-scraper-test-page/index.html
+    url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
+    html = open(url)
+    doc = Nokogiri::HTML(html)
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
