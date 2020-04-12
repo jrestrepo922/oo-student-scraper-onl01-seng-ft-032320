27fece13c284034cbf5f36ec5d28267809b3d1a1
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-12 15:32:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,7 +34,7 @@ class Scraper
         student_profile[:github] = social.attribute("href").value
       else
         student_profile[:blog] = social.attribute("href").value
-      end
+      end 
     }
 
     student_profile[:profile_quote] = index.css("div.profile-quote").text
