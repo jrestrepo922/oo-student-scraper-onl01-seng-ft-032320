b164e5e501aa5e646559c36ce4c43f3aebedfb58
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-12 14:31:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
     html = open(index_url)
     index = Nokogiri::HTML(html)
     index.css("div.student-card").each do |student|
-      binding.pry  # you got to find a css selector that contains all the studens and will iterate through all of one student information a scrape what we want
+      #binding.pry  # you got to find a css selector that contains all the studens and will iterate through all of one student information a scrape what we want
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
